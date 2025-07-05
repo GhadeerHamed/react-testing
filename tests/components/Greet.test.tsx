ffb6936f8e14cc811996ea18eabec4c8cfee937f
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-07-05 21:54:17 +0300

    Test Greet comonent 2

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -5,10 +5,19 @@ import '@testing-library/jest-dom/vitest'
 
 
 describe('Greet', () => {
-  it('renders greet message', () => {
+  it('renders greet message when name is provided', () => {
     render(<Greet name='Ghadeer'/>)
   
     const h = screen.getByRole('heading');
     expect(h).toBeInTheDocument()
+    expect(h).toHaveTextContent(/ghadeer/i)
+  })
+  
+  it('renders login button when name is not provided', () => {
+    render(<Greet />)
+  
+    const b = screen.getByRole('button');
+    expect(b).toBeInTheDocument()
+    expect(b).toHaveTextContent(/login/i)
   })
 })
