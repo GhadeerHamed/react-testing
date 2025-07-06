975f47d5dbe4e8a5857e54b4d2d635d2ae498d4f
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-07-06 10:11:22 +0300

    Remove unused imports

--- tests/components/ProductImageGallery.test.tsx
+++ tests/components/ProductImageGallery.test.tsx
@@ -1,7 +1,5 @@
 
 import { render, screen } from '@testing-library/react'
-import { User } from '../../src/entities'
-import UserList from '../../src/components/UserList'
 import ProductImageGallery from '../../src/components/ProductImageGallery'
 
 
