4e299f903e1e48380e5d6e3f41f7b6ad0e27c2f2
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-07-12 09:32:31 +0300

    test(ProductList): refactor tests to use AllProviders for rendering
    - Replace QueryClientProvider with AllProviders wrapper in ProductList tests
    - Simplify component rendering in tests for better consistency
    - Ensure all tests maintain functionality while improving readability

---
+++ tests/AllProviders.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react"
+import { QueryClient, QueryClientProvider } from "react-query"
+
+const AllProviders = ({ children }: PropsWithChildren) => {
+    const queryClient = new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      })
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
+
+export default AllProviders
--- tests/components/ProductList.test.tsx
+++ tests/components/ProductList.test.tsx
@@ -4,7 +4,7 @@ import ProductList from '../../src/components/ProductList'
 import { server } from '../mocks/server'
 import { http, HttpResponse, delay } from 'msw'
 import { db } from '../mocks/db'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import AllProviders from '../allProviders'
 
 describe('ProductList', () => {
   const ids = [1,2,3]
@@ -16,23 +16,8 @@ describe('ProductList', () => {
     db.product.deleteMany({where: { id: {in: ids}}})
   })
 
-  const renderComponent = () => {
-    const queryClient = new QueryClient({
-      defaultOptions: {
-        queries: {
-          retry: false,
-        },
-      },
-    })
-    render(
-      <QueryClientProvider client={queryClient}>
-        <ProductList />
-      </QueryClientProvider>
-      )
-  }
-  
   it('should render the list of products', async () => {
-    renderComponent()
+    render(<ProductList />, {wrapper: AllProviders})
     
     const listItems = await screen.findAllByRole('listitem')
     expect(listItems.length).toBeGreaterThan(0)
@@ -40,7 +25,7 @@ describe('ProductList', () => {
 
   it('should render no products available if no products found', async () => {
     server.use(http.get('/products', () =>  HttpResponse.json([])))
-    renderComponent()
+    render(<ProductList />, {wrapper: AllProviders})
 
     const message = await screen.findByText(/no products/i)
     expect(message).toBeInTheDocument()
@@ -48,7 +33,7 @@ describe('ProductList', () => {
 
   it('should render error message when there is an error', async () => {
     server.use(http.get('/products', () =>  HttpResponse.error()))
-    renderComponent()
+    render(<ProductList />, {wrapper: AllProviders})
 
     const message = await screen.findByText(/error/i)
     expect(message).toBeInTheDocument()
@@ -60,14 +45,14 @@ describe('ProductList', () => {
       return HttpResponse.json([])
     }))
 
-    renderComponent()
+    render(<ProductList />, {wrapper: AllProviders})
 
     const message = await screen.findByText(/loading/i)
     expect(message).toBeInTheDocument()
   })
 
   it('should remove the loading indicator when data is loaded', async () => {
-    renderComponent()
+    render(<ProductList />, {wrapper: AllProviders})
     
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
   })
@@ -75,7 +60,7 @@ describe('ProductList', () => {
   it('should remove the loading indicator if fetch data fails ', async () => {
     server.use(http.get('/products', () =>  HttpResponse.error()))
     
-    renderComponent()
+    render(<ProductList />, {wrapper: AllProviders})
     
     await waitForElementToBeRemoved(() => screen.getByText(/loading/i))
   })
